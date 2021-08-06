a15fb47b130fe5f3c069316f025097db4db28937
thomaspereira94 <thomaspereira1994@outlook.com>
2021-08-06 00:44:24 -0400

    gg

--- js/site.js
+++ js/site.js
@@ -1,4 +1,21 @@
-//call hello world
-function HelloWorld(){
-    alert("hello world");
+//get values from interface
+function getValues(){
+    //get values from page
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+
+    alert("The Start Value: " + startValue);
+    //call numberGenerator()
+
+    //call displayNumbers
+}
+
+//generate numbers from startValue to endValue
+function numberGenerator(){
+
+}
+
+//display all values, and mark even numbers bold
+function displayNumbers(){
+    
 }
